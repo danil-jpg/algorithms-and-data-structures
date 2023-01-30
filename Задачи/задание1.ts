1204644ba406ed5f78915fc9b70fbd37ad67e07b
Danil <danil.polovinkin.1994@gmail.com>
2023-01-30 11:53:48 +0300

    completed first task

---
+++ Задачи/задание1.ts
@@ -0,0 +1,40 @@
+// any
+
+let parAny: any = 3;
+parAny = "str";
+
+const str: string = "string";
+
+const num: number = 23;
+
+const bigInt = BigInt(100);
+
+const bool: boolean = true;
+
+const sym: symbol = Symbol("Key");
+
+function someFunction(): void {
+  console.log(`Void`);
+}
+
+const obj: { property: string } = { property: "foo" };
+
+const otherArr: (number | string | boolean)[] = [1, true, "string"];
+
+// tuple
+const employee: [number, string, boolean] = [1, "Steve", true];
+
+// null - ожидаемая пустота
+// undefined - неожиданная пустота
+
+enum UserResponse {
+  No = 0,
+  Yes = 1,
+}
+
+function error(message: string): never {
+  throw new Error(message);
+}
+
+let notSure: unknown = 4;
+notSure = "maybe a string instead";
