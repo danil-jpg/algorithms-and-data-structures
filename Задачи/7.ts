21de21914cfa5cfca4cb02153a86193491f8c3ba
Danil <danil.polovinkin.1994@gmail.com>
2023-02-28 15:27:10 +0300

    completed quick sort,added some drafts to diagram

--- Задачи/7.ts
+++ Задачи/7.ts
@@ -14,6 +14,47 @@ class Sort {
     console.log([arr, end]);
     return [arr, end];
   }
+
+  static quickSort(arr: number[] = []): number[] {
+    if (arr.length < 2) {
+      return arr;
+    }
+
+    const left: number[] = [];
+    const right: number[] = [];
+
+    const min = 1;
+    const max = arr.length - 1;
+    const randomInt = Math.floor(min + Math.random() * (max + 1 - min));
+
+    const pivot = arr[randomInt];
+
+    arr.slice(arr.indexOf(pivot), 1);
+
+    [pivot].concat(arr);
+
+    for (let i = 1; i < arr.length; i++) {
+      if (pivot > arr[i]) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
+    }
+    const res = this.quickSort(left).concat(pivot, this.quickSort(right));
+    return res;
+  }
+
+  static genRandNumArr(numOfElem: number): number[] {
+    const arr: number[] = [];
+    for (let i = 0; i < numOfElem; i++) {
+      const rand = Math.round(Math.random() * numOfElem);
+      arr.push(rand);
+    }
+    //   console.log(arr);
+    return arr;
+  }
 }
 
-Sort.bubleSort([1, 2, 55, 21]);
+// Sort.bubleSort([1, 2, 55, 21]);
+
+console.log(Sort.quickSort(Sort.genRandNumArr(15)));
