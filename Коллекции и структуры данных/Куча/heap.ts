17e08bac2b44d2d4c9ba22978baa8a42e4e27eee
Danil <danil.polovinkin.1994@gmail.com>
2023-03-13 12:04:55 +0300

    added heap

---
+++ Коллекции и структуры данных/Куча/heap.ts
@@ -0,0 +1,41 @@
+class Heap {
+  public heap: number[];
+
+  constructor() {
+    this.heap = [];
+  }
+
+  insertMinNum(int: number) {
+    this.heap.push(int);
+
+    let curr = this.heap.length - 1;
+
+    for (curr; curr > 0 && this.heap[Math.floor(curr / 2)] > this.heap[curr]; curr = Math.floor(curr / 2)) {
+      [this.heap[Math.floor(curr / 2)], this.heap[curr]] = [this.heap[curr], this.heap[Math.floor(curr / 2)]];
+    }
+  }
+}
+
+const heap = new Heap();
+
+heap.insertMinNum(1);
+
+heap.insertMinNum(2);
+
+heap.insertMinNum(5);
+
+heap.insertMinNum(0);
+
+heap.insertMinNum(-1);
+
+heap.insertMinNum(-10);
+
+heap.insertMinNum(-55);
+
+heap.insertMinNum(55);
+
+heap.insertMinNum(-51);
+
+heap.insertMinNum(-1);
+
+console.log(heap.heap);
